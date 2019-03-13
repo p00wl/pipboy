f5617851f5574d82d5fd6aa4484a659e5f5a663a
P00WL <paulo.ferreirabt@gmail.com>
2019-03-13 14:15:18 -0300

    update to frame.html, make this responsive

---
+++ js/pipboy.app.js
@@ -0,0 +1,13 @@
+var calcHeight = function() {
+            $('#preview-frame').height($(window).height());
+          }
+ 
+          $(document).ready(function() {
+            calcHeight();
+          }); 
+ 
+          $(window).resize(function() {
+            calcHeight();
+          }).load(function() {
+            calcHeight();
+          });
